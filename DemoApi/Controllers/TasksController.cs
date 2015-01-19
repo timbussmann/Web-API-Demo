a0a35f7fe0c9d85c4fe81239813adc96cd5f8365
timbussmann <bussmann.t@gmail.com>
2015-01-19 19:27:49 +0100

    Ex1

---
+++ DemoApi/Controllers/TasksController.cs
@@ -0,0 +1,43 @@
+﻿namespace DemoApi.Controllers
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web.Http;
+    using DemoApi.Models;
+
+    public class TasksController : ApiController
+    {
+        private static readonly List<TodoTask> Tasks = new List<TodoTask>();
+
+        public IEnumerable<TodoTask> GetAll()
+        {
+            return Tasks;
+        }
+
+        public TodoTask GetTask(int id)
+        {
+            return Tasks.SingleOrDefault(task => task.Id == id);
+        }
+
+        public int AddTask(string text)
+        {
+            int newId = Tasks.Any() 
+                ? Tasks.Select(x => x.Id).Max() + 1 
+                : 1;
+
+            Tasks.Add(new TodoTask
+            {
+                Id = newId,
+                Text = text
+            });
+
+            return newId;
+        }
+
+        public void DeleteTask(int id)
+        {
+            TodoTask task = Tasks.Find(x => x.Id == id);
+            Tasks.Remove(task);
+        }
+    }
+}
---
+++ DemoApi/Models/TodoTask.cs
@@ -0,0 +1,9 @@
+﻿namespace DemoApi.Models
+{
+    public class TodoTask
+    {
+        public int Id { get; set; }
+
+        public string Text { get; set; }
+    }
+}
