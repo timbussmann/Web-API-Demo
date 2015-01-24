00c84c05715fe72d1c640799c4b200be75e37935
timbussmann <bussmann.t@gmail.com>
2015-01-24 16:37:31 +0100

    enabling cors

--- DemoApi/App_Start/WebApiConfig.cs
+++ DemoApi/App_Start/WebApiConfig.cs
@@ -5,12 +5,16 @@ using System.Web.Http;
 
 namespace DemoApi
 {
+    using System.Web.Http.Cors;
     using Newtonsoft.Json.Serialization;
 
     public static class WebApiConfig
     {
         public static void Register(HttpConfiguration config)
         {
+            var corsEnabled = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(corsEnabled);
+
             // Web API configuration and services
             var json = GlobalConfiguration.Configuration.Formatters.JsonFormatter;
             json.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
